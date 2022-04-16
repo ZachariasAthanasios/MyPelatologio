3314f2812dc1b29cbd4448590ff84987eb7bd2fc
Zacharias Athanasios <zachariasathanasioss@gmail.com>
2022-04-16 14:07:54 +0300

    complete login page

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// Login - SignUp - Forgot Password Focus Input
+const inputs = document.querySelectorAll(".input");
+
+
+function addfocus(){
+	let parent = this.parentNode.parentNode;
+	parent.classList.add("focus");
+}
+
+function remfocus(){
+	let parent = this.parentNode.parentNode;
+	if(this.value == ""){
+		parent.classList.remove("focus");
+	}
+}
+
+
+inputs.forEach(input => {
+	input.addEventListener("focus", addfocus);
+	input.addEventListener("blur", remfocus);
+});
