a421abb1482ec3aa69c9304fcc91bcd45e031f63
Zacharias Thanos <zachariasthanoss@gmail.com>
2022-06-20 21:50:25 +0300

    Update index

--- index.php
+++ index.php
@@ -1,25 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <!-- Meta tags -->
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Αn app for Freelancers to track their sales.">
-    <meta name="keywords" content="CMS, MyCMS, HTML, CSS, JavaScript, PHP, MySQL">
-    <meta name="author" content="Zacharias Athanasios">
+	<!-- Meta tags -->
+    	<meta charset="UTF-8">
+    	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    	<meta name="description" content="Αn app for Freelancers to track their sales.">
+    	<meta name="keywords" content="CMS, MyCMS, HTML, CSS, JavaScript, PHP, MySQL">
+    	<meta name="author" content="Zacharias Athanasios">
 
-    <!-- Page Title -->
-    <title>Login | MyPelatologio</title>
+    	<!-- Page Title -->
+    	<title>Login | MyPelatologio</title>
 
 	<!-- Σύνδεση Favicon Icon -->
-    <link rel="icon" href="images/coding.jpeg" type="image/x-icon">
+    	<link rel="icon" href="images/coding.jpeg" type="image/x-icon">
 
-    <!-- Font-Awesome CDN Link 6.1.1 -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    	<!-- Font-Awesome CDN Link 6.1.1 -->
+    	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-    <!-- Custom CSS File Link -->
-    <link rel="stylesheet" href="styles/style.css">
+    	<!-- Custom CSS File Link -->
+    	<link rel="stylesheet" href="styles/style.css">
 </head>
 <body>
 	<div class="container">
@@ -67,7 +67,7 @@
         	</div>
 	</div>
 	
-    <!-- Custom JS File Link -->
-    <script type="text/javascript" src="js/main.js"></script>
+	<!-- Custom JS File Link -->
+    	<script type="text/javascript" src="js/main.js"></script>
 </body>
 </html>
